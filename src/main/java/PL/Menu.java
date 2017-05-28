0ffe357d0a950cea968b4b8de66f56f9d25a6005
goolime <goolime@gmail.com>
2017-05-28 11:53:36 +0300

    Finished Directors of Personals menus.
    Next to do is storekeeper and shop manager.
    open issues-  getters for all workers/Drivers and none special workers login.

--- src/main/java/PL/Menu.java
+++ src/main/java/PL/Menu.java
@@ -4,6 +4,7 @@ import SharedClasses.NituzException;
 import SharedClasses.MenuOP;
 import BL.*;
 import SharedClasses.Shift;
+import com.sun.org.apache.xpath.internal.operations.String;
 
 import javax.naming.SizeLimitExceededException;
 import java.util.Scanner;
@@ -32,46 +33,46 @@ public class Menu {
             MenuOP t1=new MenuOP("Workers Control");
             MenuOP t11=new MenuOP("Add Driver", ()->{
                 System.out.println("Please insert the new driver ID:");
-                String id=scanner.nextLine();
+                java.lang.String id=scanner.nextLine();
                 System.out.println("Please insert the new Driver last name:");
-                String lName=scanner.nextLine();
+                java.lang.String lName=scanner.nextLine();
                 System.out.println("Please insert the new Driver first name:");
-                String fName=scanner.nextLine();
+                java.lang.String fName=scanner.nextLine();
                 System.out.println("Please insert the new Driver starting date:");
-                String startingDate=scanner.nextLine();
+                java.lang.String startingDate=scanner.nextLine();
                 System.out.println("Please insert the new Driver license type:");
-                String licens=scanner.nextLine();
+                java.lang.String licens=scanner.nextLine();
                 System.out.println("Please insert the new Driver salary:");
-                String salary=scanner.nextLine();
+                java.lang.String salary=scanner.nextLine();
                 System.out.println("Please insert the new Driver bank number:");
-                String bankn=scanner.nextLine();
+                java.lang.String bankn=scanner.nextLine();
                 System.out.println("Please insert the new Driver account number:");
-                String acountn=scanner.nextLine();
+                java.lang.String acountn=scanner.nextLine();
                 System.out.println("Please insert the new Driver terms:");
-                String terms=scanner.nextLine();
+                java.lang.String terms=scanner.nextLine();
                 bl.addDriver(id,lName,fName,startingDate,terms,salary,licens,bankn,acountn);
             });
             t1.addSon(t11);
             MenuOP t12=new MenuOP("Update Driver",()->{
                 System.out.println("Please insert the ID of driver that you wants to update:");
-                String id=scanner.nextLine();
+                java.lang.String id=scanner.nextLine();
                 System.out.println("Please insert the Driver new last name (if there is no change leave empty):");
-                String lName=scanner.nextLine();
+                java.lang.String lName=scanner.nextLine();
                 System.out.println("Please insert the Driver new first name (if there is no change leave empty):");
-                String fName=scanner.nextLine();
+                java.lang.String fName=scanner.nextLine();
                 System.out.println("Please insert the Driver new starting date (if there is no change leave empty):");
-                String startingDate=scanner.nextLine();
+                java.lang.String startingDate=scanner.nextLine();
                 System.out.println("Please insert the Driver new license type (if there is no change leave empty):");
-                String licens=scanner.nextLine();
+                java.lang.String licens=scanner.nextLine();
                 System.out.println("Please insert the Driver new salary (if there is no change leave empty):");
-                String salary=scanner.nextLine();
+                java.lang.String salary=scanner.nextLine();
                 System.out.println("Please insert the Driver new bank number (if there is no change leave empty):");
-                String bankn=scanner.nextLine();
+                java.lang.String bankn=scanner.nextLine();
                 System.out.println("Please insert the Driver new account number (if there is no change leave empty):");
-                String acountn=scanner.nextLine();
+                java.lang.String acountn=scanner.nextLine();
                 System.out.println("Please insert the Driver new terms (if there is no change leave empty):");
-                String terms=scanner.nextLine();
-                String d[]=new String[9];
+                java.lang.String terms=scanner.nextLine();
+                java.lang.String d[]=new  java.lang.String[9];
                 if(!lName.contentEquals("")){
                     d[0]=lName;
                 }
@@ -101,10 +102,14 @@ public class Menu {
             t1.addSon(t12);
             MenuOP t13=new MenuOP("Remove Driver",()->{
                 System.out.println("Please insert the ID of Driver you wants to Remove:");
-                String id=scanner.nextLine();
+                java.lang.String id=scanner.nextLine();
                 bl.removeDriver(id);
             });
             t1.addSon(t13);
+            MenuOP t14=new MenuOP("Show All Drivers",()->{
+                System.out .println();// TODO: Ofir & Liam : add needed function
+            });
+            t1.addSon(t14);
             menues[0].addSon(t1);
             MenuOP t2=new MenuOP("Workers Shifts");
             MenuOP t21=new MenuOP("Open Shift new Shift",()->{
@@ -133,7 +138,7 @@ public class Menu {
                 System.out.println(bl.getShift(date,time).toString());
             });
             t2.addSon(t23);
-            MenuOP t24=new MenuOP("Manage workers in Shifts");
+            MenuOP t24=new MenuOP("Manage Workers in Shifts");
                 MenuOP t241=new MenuOP("Show Available Worker",()->{
                     System.out.println("Please Insert the date of the Shift:");
                     String date=this.scanner.nextLine();
@@ -143,9 +148,9 @@ public class Menu {
                 });
                 t24.addSon(t241);
                 MenuOP t242=new MenuOP("Add Workers to Shift",()->{
-                    System.out.println("Please Insert the date of the Shift you wants to Delete:");
+                    System.out.println("Please Insert the date of the Shift:");
                     String date=this.scanner.nextLine();
-                    System.out.println("Please Insert the time of the Shift you wants to Delete:");
+                    System.out.println("Please Insert the time of the Shift:");
                     String time=this.scanner.nextLine();
                     Shift s=bl.getShift(date,time);
                     boolean con=true;
@@ -159,7 +164,234 @@ public class Menu {
                     }
                 });
                 t24.addSon(t242);
+                MenuOP t243=new MenuOP("Remove Workers from Shift",()->{
+                    System.out.println("Please Insert the date of the Shift:");
+                    String date=this.scanner.nextLine();
+                    System.out.println("Please Insert the time of the Shift:");
+                    String time=this.scanner.nextLine();
+                    Shift s=bl.getShift(date,time);
+                    boolean con=true;
+                    while(con) {
+                        System.out.println("Please insert Worker ID or 'q' if you have finished");
+                        String id=scanner.nextLine();
+                        if (id.contentEquals("q"))
+                            con=false;
+                        else
+                            bl.removeWorkerFromShift(id, s);
+                    }
+                });
+                t24.addSon(t243);
+                MenuOP t244=new MenuOP("Sign Worker to Shift",()->{
+                    System.out.println("Please Insert the date of the Shift:");
+                    java.lang.String date=this.scanner.nextLine();
+                    System.out.println("Please Insert the time of the Shift:");
+                    java.lang.String time=this.scanner.nextLine();
+                    Shift s=bl.getShift(date,time);
+                    boolean con=true;
+                    while(con) {
+                        System.out.println("Please insert Worker ID or 'q' if you have finished");
+                        java.lang.String id=scanner.nextLine();
+                        if (id.contentEquals("q"))
+                            con=false;
+                        else
+                            bl.addPossibleShift(s,id);
+                    }
+                });
+                t24.addSon(t244);
+                MenuOP t245=new MenuOP ("Unsign Worker from Shift",()->{
+                    System.out.println("Please Insert the date of the Shift:");
+                    String date=this.scanner.nextLine();
+                    System.out.println("Please Insert the time of the Shift:");
+                    String time=this.scanner.nextLine();
+                    Shift s=bl.getShift(date,time);
+                    boolean con=true;
+                    while(con) {
+                        System.out.println("Please insert Worker ID or 'q' if you have finished");
+                        String id=scanner.nextLine();
+                        if (id.contentEquals("q"))
+                            con=false;
+                        else
+                            bl.deletePossibleShift(s,id);
+                    }
+                });
+                t24.addSon(t245);
+            t2.addSon(t24);
+            MenuOP t25=new MenuOP("Manege Roles in Shifts");
+                MenuOP t251=new MenuOP("Add Roles to Shift",()->{
+                    System.out.println("Please Insert the date of the Shift:");
+                    java.lang.String date=this.scanner.nextLine();
+                    System.out.println("Please Insert the time of the Shift:");
+                    java.lang.String time=this.scanner.nextLine();
+                    Shift s=bl.getShift(date,time);
+                    boolean con=true;
+                    while(con) {
+                        System.out.println("Please insert Role or 'q' if you have finished");
+                        java.lang.String role=scanner.nextLine();
+                        if (role.contentEquals("q"))
+                            con=false;
+                        else {
+                            java.lang.String r[] = new java.lang.String[1];
+                            r[0]=role;
+                            bl.chooseRolesInShift(r,s);
+                        }
+                    }
+                });
+                t25.addSon(t251);
+                MenuOP t252=new MenuOP("Remove Role from Shift",()->{
+                    System.out.println("Please Insert the date of the Shift:");
+                    java.lang.String date=this.scanner.nextLine();
+                    System.out.println("Please Insert the time of the Shift:");
+                    java.lang.String time=this.scanner.nextLine();
+                    Shift s=bl.getShift(date,time);
+                    boolean con=true;
+                    while(con) {
+                        System.out.println("Please insert Role or 'q' if you have finished");
+                        java.lang.String role=scanner.nextLine();
+                        if (role.contentEquals("q"))
+                            con=false;
+                        else
+                            bl.deleteRoleInShift(role, s);
+                    }
+                });
+                t25.addSon(t252);
+            t2.addSon(t25);
+            menues[0].addSon(t2);
+            MenuOP t3=new MenuOP("Banks Control");
+                MenuOP t31=new MenuOP ("Add Bank",()->{
+                    System.out.println("Please enter Bank Number:");
+                    java.lang.String bnum=scanner.nextLine();
+                    System.out.println("Please enter Bank Name:");
+                    java.lang.String bname=scanner.nextLine();
+                    bl.addBank(bnum,bname);
+                });
+                t3.addSon(t31);
+                MenuOP t32=new MenuOP ("Remove Bank",()->{
+                    System.out.println("Please enter Bank Number:");
+                    java.lang.String bnum=scanner.nextLine();
+                    bl.deleteBank(bnum);
+                });
+                t3.addSon(t32);
+                MenuOP t33=new MenuOP ("Show All Banks",()->{
+                    System.out.println();//TODO: ofir & liam: add needed function
+                });
+                t3.addSon(t33);
+            menues[0].addSon(t3);
+            MenuOP t4=new MenuOP("Roles Management");
+                MenuOP t41=new MenuOP("Add Role",()->{
+                    boolean con=true;
+                    while(con) {
+                        System.out.println("Please Role Name or 'q' if you have finished");
+                        java.lang.String id=scanner.nextLine();
+                        if (id.contentEquals("q"))
+                            con=false;
+                        else
+                            bl.addRole(id);
+                    }
+                });
+                t4.addSon(t41);
+                MenuOP t42=new MenuOP("Remove Role",()->{
+                    boolean con=true;
+                    while(con) {
+                        System.out.println("Please insert Role Name or 'q' if you have finished");
+                        java.lang.String id=scanner.nextLine();
+                        if (id.contentEquals("q"))
+                            con=false;
+                        else
+                            bl.deleteRole(id);
+                    }
+                });
+                t4.addSon(t42);
+                MenuOP t43=new MenuOP("Show Roles",()->{
+                    System.out.println();//TODO: Ofir & Liam: add needed function
+                });
+                t4.addSon(t43);
+            menues[0].addSon(t4);
+
         menues[1]=new MenuOP("Director of Personnel shop");
+        MenuOP s1=new MenuOP("Workers Control");
+            MenuOP s11=new MenuOP("Add Worker",()->{
+                System.out.println("Please insert the new Worker ID:");
+                java.lang.String id=scanner.nextLine();
+                System.out.println("Please insert the new Worker last name:");
+                java.lang.String lName=scanner.nextLine();
+                System.out.println("Please insert the new Worker first name:");
+                java.lang.String fName=scanner.nextLine();
+                System.out.println("Please insert the new Worker starting date:");
+                java.lang.String startingDate=scanner.nextLine();
+                System.out.println("Please insert the new Worker job:");
+                java.lang.String licens=scanner.nextLine();
+                System.out.println("Please insert the new Worker salary:");
+                java.lang.String salary=scanner.nextLine();
+                System.out.println("Please insert the new Worker bank number:");
+                java.lang.String bankn=scanner.nextLine();
+                System.out.println("Please insert the new Worker account number:");
+                java.lang.String acountn=scanner.nextLine();
+                System.out.println("Please insert the new Worker terms:");
+                java.lang.String terms=scanner.nextLine();
+                bl.add(id,lName,fName,startingDate,terms,salary,licens,bankn,acountn);
+            });
+            s1.addSon(s11);
+            MenuOP s12=new MenuOP("Update Worker",()->{
+                System.out.println("Please insert the ID of the Worker that you wants to update:");
+                java.lang.String id=scanner.nextLine();
+                System.out.println("Please insert the Worker new last name (if there is no change leave empty):");
+                java.lang.String lName=scanner.nextLine();
+                System.out.println("Please insert the Worker new first name (if there is no change leave empty):");
+                java.lang.String fName=scanner.nextLine();
+                System.out.println("Please insert the Worker new starting date (if there is no change leave empty):");
+                java.lang.String startingDate=scanner.nextLine();
+                System.out.println("Please insert the Worker new job (if there is no change leave empty):");
+                java.lang.String licens=scanner.nextLine();
+                System.out.println("Please insert the Worker new salary (if there is no change leave empty):");
+                java.lang.String salary=scanner.nextLine();
+                System.out.println("Please insert the Worker new bank number (if there is no change leave empty):");
+                java.lang.String bankn=scanner.nextLine();
+                System.out.println("Please insert the Worker new account number (if there is no change leave empty):");
+                java.lang.String acountn=scanner.nextLine();
+                System.out.println("Please insert the Worker new terms (if there is no change leave empty):");
+                java.lang.String terms=scanner.nextLine();
+                java.lang.String d[]=new  java.lang.String[9];
+                if(!lName.contentEquals("")){
+                    d[0]=lName;
+                }
+                if(!fName.contentEquals("")){
+                    d[1]=fName;
+                }
+                if(!startingDate.contentEquals("")){
+                    d[2]=startingDate;
+                }
+                if(!terms.contentEquals("")){
+                    d[3]=terms;
+                }
+                if(!salary.contentEquals("")){
+                    d[4]=salary;
+                }
+                if(!licens.contentEquals("")){
+                    d[5]=licens;
+                }
+                if(!bankn.contentEquals("")){
+                    d[6]=bankn;
+                }
+                if(!acountn.contentEquals("")){
+                    d[7]=acountn;
+                }
+                bl.update(id,d);
+            });
+            s1.addSon(s12);
+            MenuOP s13=new MenuOP("Remove Worker",()->{
+                System.out.println("Please insert the ID of the Worker you wants to Remove:");
+                java.lang.String id=scanner.nextLine();
+                bl.remove(id);
+            });
+            s1.addSon(s13);
+            MenuOP s14=new MenuOP("Show All Workers",()->{
+                System.out .println();// TODO: Ofir & Liam : add needed function
+            });
+            s1.addSon(s14);
+        menues[1].addSon(s1);
+        menues[1].addSon(t2.clone());
+        menues[1].addSon(t3.clone());
+        menues[1].addSon(t4.clone());
         menues[2]=new MenuOP("Director of logistics");
             //for Director of Logistics
             MenuOP l1=new MenuOP("Trucks Control");
