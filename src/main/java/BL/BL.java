595f589be1b6f03a94615a0093150b0c54478d39
goolime <goolime@gmail.com>
2017-05-31 22:01:34 +0300

    done what was needed

--- src/main/java/BL/BL.java
+++ src/main/java/BL/BL.java
@@ -1216,8 +1216,10 @@ public class BL {
 	 */
 	public boolean clearTransport(int orderID) throws NituzException
 	{
-		//TODO : gal
-
-		return true;
+		if(dal.isTransport(orderID)){
+			dal.zeroiesTransport(orderID);
+			return true;
+		}
+		return false;
 	}
 }
